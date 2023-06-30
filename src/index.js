9cf513de00576c3a6ae94502f1f72a7fec497719
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-06-30 17:29:50 +0300

    npm init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-console.warn("start app");
+console.warn("start app", 2 + 3);
+
+const fs = require("fs");
+const path = require("path");
+
+// fs.readdir(
+//     "C:\Users\AlexandraBichir\OneDrive - TEC Software Solutions\Desktop\CURS\inregistrari";
+//     (err, files) => {
+//   console.warn(files);
+// });
 
 // const person = {
 //   firstName: "Alexandra",
