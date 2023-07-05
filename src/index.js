d516368a2ae3cc1efd8b883a27acddd652cbd74d
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-07-05 18:16:31 +0300

    load and render teams

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import "./style.css";
 
-console.warn("start app!");
+function getTeamAsHTML(team) {
+  return `<tr>
+  <td>${team.promotion}</td>
+  <td>${team.members}</td>
+  <td>${team.name}</td>
+  <td><a href="${team.url}">${team.aliasurl}</a></td>
+  <td>✖ ✎</td>
+</tr>`;
+}
+
+function renderTeams(teams) {
+  const htmlTeams = teams.map(getTeamAsHTML);
+  console.warn(htmlTeams);
+  document.querySelector("#teamsTable tbody").innerHTML = htmlTeams.join("");
+}
+function loadTeams() {
+  fetch("teams.json")
+    .then(r => r.json())
+    .then(renderTeams);
+}
+
+loadTeams();
