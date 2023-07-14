e92285631c403c0740a6ed4b9a12a4e1f0947dd9
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-07-14 17:55:21 +0300

    implement filter elements

--- src/index.js
+++ src/index.js
@@ -163,7 +163,20 @@ function setInputsDisable(disabled) {
   });
 }
 
+function filterElements(teams, search) {
+  return teams.filter(team => {
+    //console.info("search %o in %o", search, team.promotion);
+    return team.promotion.includes(search);
+  });
+}
+
 function initEvents() {
+  $("#search").addEventListener("input", e => {
+    const search = e.target.value;
+    const teams = filterElements(allTeams, search);
+    //console.info("search", search, teams);
+    renderTeams(teams);
+  });
   $("#teamsForm").addEventListener("submit", onSubmit);
   $("#teamsForm").addEventListener("reset", e => {
     console.info("reset", editId);
