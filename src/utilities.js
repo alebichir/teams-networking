ce1181d902f19a2c3ffb712c145ff0b01a630aff
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-08-02 20:44:02 +0300

    export/import mask, $, unmask functions

---
+++ src/utilities.js
@@ -0,0 +1,19 @@
+export function $(selector) {
+  return document.querySelector(selector);
+}
+
+export function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+export function mask(el) {
+  el.classList.add("loading-mask");
+}
+
+export function unmask(el) {
+  el.classList.remove("loading-mask");
+}
