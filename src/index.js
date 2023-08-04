71d8ac707975e167b002d32595539f9e71808e3a
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-08-04 18:43:20 +0300

    forcer render after cancel edit (pass -1 as editId)

--- src/index.js
+++ src/index.js
@@ -198,7 +198,7 @@ function initEvents() {
     if (editId) {
       //console.warn("cancel edit");
       allTeams = [...allTeams];
-      renderTeams(allTeams);
+      renderTeams(allTeams, -1); // use -1 to force render
       setInputsDisable(false);
       editId = "";
     }
